12b03e52ae349f3c838b0478ae8d200874213796
Ildefonso Lares <65493114+Flayxamax@users.noreply.github.com>
2024-02-07 09:22:05 -0700

    Add: Se agregó el proyecto

---
+++ SlackMockUp_233381_233267/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SlackMockUp_233381_233267"
+include(":app")
+ 
