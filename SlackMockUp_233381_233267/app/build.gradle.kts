62709d30cf9b57abcf8c944bdc371da1fa108053
Ildefonso Lares <65493114+Flayxamax@users.noreply.github.com>
2024-02-08 17:48:21 -0700

    Update: Se actualizó la vista

--- SlackMockUp_233381_233267/app/build.gradle.kts
+++ SlackMockUp_233381_233267/app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
         jvmTarget = "1.8"
     }
     buildFeatures {
+        viewBinding = true
         compose = true
     }
     composeOptions {
@@ -60,6 +61,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- SlackMockUp_233381_233267/app/src/main/java/com/example/slackmockup_233381_233267/MainActivity.kt
+++ SlackMockUp_233381_233267/app/src/main/java/com/example/slackmockup_233381_233267/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.slackmockup_233381_233267
 
 import android.os.Bundle
+import android.widget.SearchView
 import androidx.activity.ComponentActivity
+import com.example.slackmockup_233381_233267.databinding.ActivityMainBinding
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -10,37 +12,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import com.example.slackmockup_233381_233267.ui.theme.SlackMockUp_233381_233267Theme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            SlackMockUp_233381_233267Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        val searchView = binding.searchEditText
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SlackMockUp_233381_233267Theme {
-        Greeting("Android")
+        searchView.queryHint = "Jump to or search..."
+
     }
 }
